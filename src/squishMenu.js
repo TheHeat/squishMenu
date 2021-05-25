20d1c8263f34c4bdbfae09d61a21095cd557f00c
Bones <ben@properdesign.co.uk>
2021-05-25 13:29:41 +0100

    swaps console errors for logs

--- src/squishMenu.js
+++ src/squishMenu.js
@@ -1,18 +1,18 @@
-import getItemsWidth from './getItemsWidth';
+import getItemsWidth from "./getItemsWidth";
 
 // squishMenu
 const squishMenu = (options) => {
   const container = document.getElementById(options.containerId);
 
   if (container === undefined) {
-    console.error('containerId is undefined');
+    console.log("containerId is undefined");
   } else if (container === null) {
-    console.error('containerId is not available');
+    console.log("containerId is not available");
   } else {
     const itemsWidth = getItemsWidth(container, () => {
       // After we've calculated the width of all the .menu-items
       // add class .squish-ready to the container
-      container.classList.add('squish-ready');
+      container.classList.add("squish-ready");
     });
 
     // Set appropriate classes
@@ -20,29 +20,30 @@ const squishMenu = (options) => {
       const containerWidth = container.offsetWidth;
 
       if (itemsWidth <= containerWidth) {
-        container.classList.remove('too-small');
-        container.classList.remove('is-open');
+        container.classList.remove("too-small");
+        container.classList.remove("is-open");
       }
 
       if (itemsWidth > containerWidth) {
-        container.classList.add('too-small');
+        container.classList.add("too-small");
       }
     };
 
     setStates();
 
-    window.addEventListener('resize', setStates);
+    window.addEventListener("resize", setStates);
 
     const toggles = document.getElementsByClassName(options.toggleClass);
 
     if (toggles.length > 0) {
       // Click the .menu-toggle to open the menu. Obvs.
-      document.querySelectorAll(`.${options.toggleClass}`).forEach(item =>
-        item.addEventListener('click', () => {
-          container.classList.toggle('is-open');
-        }));
+      document.querySelectorAll(`.${options.toggleClass}`).forEach((item) =>
+        item.addEventListener("click", () => {
+          container.classList.toggle("is-open");
+        })
+      );
     } else {
-      console.error('No toggleClass found or toggleClass is undefined');
+      console.log("No toggleClass found or toggleClass is undefined");
     }
   }
 };
